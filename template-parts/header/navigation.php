5e470916d47f5983007c3090c065d3854a95ecf2
Lukas Nacke <git@nacke.it>
2026-01-18 22:45:57 +0100

    Ausrichtung der Navigation angepasst.

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -7,9 +7,11 @@
 <nav class="site-navigation primary-navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'el-team-theme' ); ?>">
 	<?php
 	wp_nav_menu( array(
-		'theme_location' => 'primary',
-		'menu_class'     => 'menu',
-		'fallback_cb'    => 'wp_page_menu',
+		'theme_location'  => 'primary',
+		'menu_class'      => 'menu',
+		'container'       => false,
+		'fallback_cb'     => 'wp_page_menu',
+		'depth'           => 2,
 	) );
 	?>
 </nav>
